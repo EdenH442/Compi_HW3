9ea7e479b66d831add9ac509b966357c67c5b10b
daniella aharon <daniella@clairlabs.com>
2022-12-12 17:24:46 +0200

    pp

--- SymbolTable.h
+++ SymbolTable.h
@@ -5,23 +5,26 @@
 #ifndef COMPI_HW3_SYMBOLTABLE_H
 #define COMPI_HW3_SYMBOLTABLE_H
 
-enum ExpType {
-    INT_EXP,
-    BYTE_EXP,
-    BOOL_EXP,
-    STRING_EXP
-};
+#include <stack>
 
-class SymbolTable {
+class symTableBlkEntry
+{
 public:
+    int offset;
+    string type;
+    string id;
+    std::vector<string> types_vec;
+    std::vector<string> names_vec;
+    bool null_offset;
+    bool is_func = false;
 
-private:
-    struct SymEntry {
-        string name;
-        ExpType type;
-        int offset;
-    };
+    symTableBlkEntry() = default;
 
+    symTableBlkEntry(int offset, string id, string type, bool null_offset, bool is_func) :
+            offset(offset), id(id), type(type),null_offset(null_offset), is_func(is_func){}
+
+    symTableBlkEntry(int offset, string id, std::vector<string> names_vec, std::vector<string> types_vec, string type, bool null_offset, bool is_func) :
+            offset(offset), id(id),names_vec(names_vec), types_vec(types_vec), type(type), null_offset(null_offset), is_func(is_func){}
 };
 
 
